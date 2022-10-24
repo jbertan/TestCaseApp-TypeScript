705a96acec138cc856f961d9090b42a96407f9f1
bertanakarsu <bertanakarsu@gmail.com>
2022-10-24 20:00:02 +0300

    Head Section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import type { AppProps } from "next/app";
 import "../styles/global.css";
 import { DataContextProvider } from "../store/data-context";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <DataContextProvider>
-      <Component {...pageProps} />
-    </DataContextProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+      </Head>
+      <DataContextProvider>
+        <Component {...pageProps} />
+      </DataContextProvider>
+    </>
   );
 }
 
